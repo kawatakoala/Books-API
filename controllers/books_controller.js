04b043ca3557bf9ff6007fe61a66494c9998f674
Michael Kawata <michaelkawata@bens-air.lan>
2023-02-12 18:38:07 -0800

    everything works

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,70 +1,9 @@
+// DEPENDENCIES
 const express = require('express')
 const books = express.Router()
 const Book = require('../models/book.js')
 
-books.get('/', (req, res) => {
-    Book.find()
-        .then(foundBooks => {
-            res.status(200).json(foundBooks)
-        })
-        .catch(err => {
-            res.status(400).json({
-                message: 'Error 404'
-            })
-        })
-})
-
-books.get('/books/:id', () => {
-    books.findById(req.params.id)
-        .then(foundBooks => {
-            res.status(200).json(foundBook)
-        })
-        .catch(err => {
-            res.status(400).json({
-                message: 'Error 404'
-            })
-        })
-})
-
-books.put('/:id', (req, res) => {
-    Book.findByIdAndUpdate(req.params.id, req.body)
-        .then(updatedBook => {
-            console.log(req.body)
-            res.status(200).json(updatedBook)
-        })
-        .catch(err => {
-            res.status(400).json({
-                message: 'An error has occurred, could not edit the book'
-            })
-        })
-})
-
-books.delete('/:id', (req, res) => {
-    Book.findByIdAndDelete(req.params.id)
-        .then(deletedBook => {
-            res.status(200).json({
-                message: 'Delete successful'
-            })
-        })
-        .catch(err => {
-            res.status(400).json({
-                message: 'An error has occurred, could not delete the book'
-            })
-        })
-})
-
-books.post('/', (req, res) => {
-    Book.create(req.body)
-        .then(createdBook => {
-            res.status(200).json(createdBook)
-        })
-        .catch(err => {
-            res.status(400).json({
-                message: 'An error has occurred, could not create the book'
-            })
-        })
-})
-
+// SEED DATA
 books.get('/seed', (req, res) => {
     Book.insertMany([{
         "title": "The Shinobi Initiative",
@@ -88,7 +27,7 @@ books.get('/seed', (req, res) => {
         "imageURL": "https://imgur.com/VGyUtrr.jpeg"
     },
     {
-        "title": "Wâˆ€RP",
+        "title": "W∀RP",
         "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
         "year": 2010,
         "quantity": 4,
@@ -102,5 +41,73 @@ books.get('/seed', (req, res) => {
         }))
 })
 
+// INDEX
+books.get('/', (req, res) => {
+    Book.find()
+        .then(foundBooks => {
+            res.status(200).json(foundBooks)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'An error has occurred, could not get all books'
+            })
+        })
+})
 
+// SHOW
+books.get('/:id', (req, res) => {
+    Book.findById(req.params.id)
+        .then(foundBook => {
+            res.status(200).json(foundBook)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'An error has occurred, could not find the book'
+            })
+        })
+})
+
+// UPDATE 
+books.put('/:id', (req, res) => {
+    Book.findByIdAndUpdate(req.params.id, req.body)
+        .then(updatedBook => {
+            console.log(req.body)
+            res.status(200).json(updatedBook)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'An error has occurred, could not edit the book'
+            })
+        })
+})
+
+// DELETE 
+books.delete('/:id', (req, res) => {
+    Book.findByIdAndDelete(req.params.id)
+        .then(deletedBook => {
+            res.status(200).json({
+                message: 'Delete successful'
+            })
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'An error has occurred, could not delete the book'
+            })
+        })
+})
+
+// CREATE 
+books.post('/', (req, res) => {
+    Book.create(req.body)
+        .then(createdBook => {
+            res.status(200).json(createdBook)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'An error has occurred, could not create the book'
+            })
+        })
+})
+
+// EXPORT
 module.exports = books
