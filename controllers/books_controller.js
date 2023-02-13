5309456003e38a897c75868154f7044bdc547577
Michael Kawata <michaelkawata@bens-air.lan>
2023-02-12 16:16:25 -0800

    books show route

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -13,3 +13,15 @@ books.get('/', (req, res) => {
             })
         })
 })
+
+books.get('/books/:id', () => {
+    books.findById(req.params.id)
+        .then(foundBooks => {
+            res.status(200).json(foundBooks)
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: 'Error 404'
+            })
+        })
+})
